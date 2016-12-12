2c6f577997d16d665a5e6288a3bd0a826fba1159
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-12 20:59:00 +0100

    Solved problem 001.

--- Problems/001 Multiples of 3 and 15/Multiples of 3 and 5/Multiples of 3 and 5/Program.cs
+++ Problems/001 Multiples of 3 and 15/Multiples of 3 and 5/Multiples of 3 and 5/Program.cs
@@ -8,9 +8,22 @@ namespace Multiples_of_3_and_5
 {
     class Program
     {
+        private const int MaxInteger = 1000;
+
         static void Main(string[] args)
         {
+            int sum = 0;
 
+            for (int integer = 0; integer < MaxInteger; integer++)
+            {
+                if(integer%3==0||integer%5==0)
+                {
+                    sum += integer;
+                }
+            }
+
+            Console.WriteLine("Sum: " + sum);
+            Console.ReadKey();
 
         }
     }
