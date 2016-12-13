70efafe4ad09333ae73a92498478cfe439c451d0
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-13 01:01:48 +0100

    002: Test class for NumberAdder generated.

---
+++ Problems/002 Even Fibonacci numbers/Even Fibonacci numbers/Even Fibonacci numbers/Test/NumberAdderTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Even_Fibonacci_numbers.Test
+{
+    [TestClass]
+    public class NumberAdderTest
+    {
+        [TestMethod]
+        public void OnlyAOne()
+        {
+            int expected = 0;
+            List<int> onlyOne = new List<int> {1};
+
+            int actual = NumberAdder.SumOfEvenNumbers(onlyOne);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
