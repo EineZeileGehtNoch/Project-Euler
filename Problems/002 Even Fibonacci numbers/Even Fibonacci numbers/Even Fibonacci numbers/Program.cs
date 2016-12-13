ea543964edba714b33408922255dd66038b51adb
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-13 16:13:10 +0100

    002: Added problem description.

--- Problems/002 Even Fibonacci numbers/Even Fibonacci numbers/Even Fibonacci numbers/Program.cs
+++ Problems/002 Even Fibonacci numbers/Even Fibonacci numbers/Even Fibonacci numbers/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//https://projecteuler.net/problem=3
+
+//Largest prime factor
+//Problem 3
+
+//The prime factors of 13195 are 5, 7, 13 and 29.
+
+//What is the largest prime factor of the number 600851475143 ?
+
+
 namespace Even_Fibonacci_numbers
 {
     class Program
