81718b36c48b7acb35467198c60361e23ddde9db
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-13 16:14:13 +0100

    001: Added problem description.

--- Problems/001 Multiples of 3 and 15/Multiples of 3 and 5/Multiples of 3 and 5/Program.cs
+++ Problems/001 Multiples of 3 and 15/Multiples of 3 and 5/Multiples of 3 and 5/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//https://projecteuler.net/problem=1
+
+//Multiples of 3 and 5
+//Problem 1
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+
 namespace Multiples_of_3_and_5
 {
     class Program
