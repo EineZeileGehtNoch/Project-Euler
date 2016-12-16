f301e14f09161f021bd503908fa889993834225f
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-17 00:06:48 +0100

    001: Added NoFactors TestMethod in AdderTest class and rewrote IsDivisibleByAnyFactor in Adder class.

--- Problems/001 Multiples of 3 and 15/Multiples of 3 and 5/Multiples of 3 and 5/AdderTest.cs
+++ Problems/001 Multiples of 3 and 15/Multiples of 3 and 5/Multiples of 3 and 5/AdderTest.cs
@@ -8,7 +8,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Multiples_of_3_and_5
 {
     [TestClass]
-    class AdderTest
+    public class AdderTest
     {
+        [TestMethod]
+        public void TrivalFactor()
+        {
+            int numberToFactor = 10;
+            List<int> factor = new List<int> {1};
+            int expected = 45;
+
+            int actual = Adder.SumOfNumbersBelowDivisibleByAnyFactor(numberToFactor, factor);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void NoFactors()
+        {
+            int numberToFactor = 10;
+            List<int> factor = new List<int>();
+            int expected = 0;
+
+            int actual = Adder.SumOfNumbersBelowDivisibleByAnyFactor(numberToFactor, factor);
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
