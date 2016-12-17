cb70c4eee529aca2fd6c7ac991bf02411fd88f73
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-17 20:38:26 +0100

    004: Created TrivialPalindrome TestMethod.

--- Problems/004 Largest palindrome product/Largest palindrome product/Largest palindrome product/Test/PalindromeTest.cs
+++ Problems/004 Largest palindrome product/Largest palindrome product/Largest palindrome product/Test/PalindromeTest.cs
@@ -3,7 +3,19 @@
 namespace Largest_palindrome_product.Test
 {
     [TestClass]
-    class PalindromeTest
+    public class PalindromeTest
     {
+
+        [TestMethod]
+        public void TrivialPalindrome()
+        {
+            int number = 1;
+            bool expected = true;
+
+            bool actual = Palindrome.NumberIsPalindrome(number);
+
+            Assert.AreEqual(expected,actual);
+        }
+
     }
 }
