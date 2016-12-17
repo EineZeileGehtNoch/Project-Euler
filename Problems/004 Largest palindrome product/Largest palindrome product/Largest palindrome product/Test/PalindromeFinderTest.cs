d7317b28070fe9666208a8ce419b2f320e8db423
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-17 23:37:49 +0100

    004: Wrote BiggestPalindromeOutOfDoubleDigitFactors TestMethod.

--- Problems/004 Largest palindrome product/Largest palindrome product/Largest palindrome product/Test/PalindromeFinderTest.cs
+++ Problems/004 Largest palindrome product/Largest palindrome product/Largest palindrome product/Test/PalindromeFinderTest.cs
@@ -1,14 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Largest_palindrome_product.Test
 {
     [TestClass]
     public class PalindromeFinderTest
     {
+        [TestMethod]
+        public void BiggestPalindromeOutOfSingleDigitFactors()
+        {
+            int numberOfDigits = 1;
+            int expected = 3*3;
+
+            int actual = PalindromeFinder.GetLargestPalindromeWhichIsProdukt(numberOfDigits);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void BiggestPalindromeOutOfDoubleDigitFactors()
+        {
+            int numberOfDigits = 2;
+            int expected = 91*99;
+
+            int actual = PalindromeFinder.GetLargestPalindromeWhichIsProdukt(numberOfDigits);
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
