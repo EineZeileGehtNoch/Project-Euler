897f682267a2e0251ff23e42ef8a830c0044565c
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-17 18:06:08 +0100

    004: Created solution.

---
+++ Problems/004 Largest palindrome product/Largest palindrome product/Largest palindrome product/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://projecteuler.net/problem=4
+
+//Largest palindrome product
+//Problem 4
+
+//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+namespace Largest_palindrome_product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
