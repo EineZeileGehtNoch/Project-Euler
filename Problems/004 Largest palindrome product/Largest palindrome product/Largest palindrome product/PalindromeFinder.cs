9e266294c4cf6f33d7f5edebb92fc8b8f2f9ef26
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-17 23:35:40 +0100

    004:Wrote GetLargestPalinDromeWhichIsProduct Method and removed Bug (TestMethod BiggestPalindromeOutOfSingleDigitFactors failed).

--- Problems/004 Largest palindrome product/Largest palindrome product/Largest palindrome product/PalindromeFinder.cs
+++ Problems/004 Largest palindrome product/Largest palindrome product/Largest palindrome product/PalindromeFinder.cs
@@ -1,16 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Largest_palindrome_product
+﻿namespace Largest_palindrome_product
 {
     static class PalindromeFinder
     {
-        public static int GetLargestPalindromeWhichIsProdukt(int numberOfFactorDigits)
+        private static int _digitsPerFactor;
+        private static int _palindrome;
+        private static int _lowerBoundFactor;
+        private static int _upperBoundFactor;
+
+        public static int GetLargestPalindromeWhichIsProdukt(int digitsPerFactor)
         {
-            return 1;
+            _digitsPerFactor = digitsPerFactor;
+
+            FindLargestPalindrom();
+
+            return _palindrome;
+        }
+
+        private static void FindLargestPalindrom()
+        {
+            SetBoundsForFactors();
+
+            TryAllFactorCombinations();
+        }
+
+        private static void TryAllFactorCombinations()
+        {
+            for (int smallerFactor = _lowerBoundFactor; smallerFactor <= _upperBoundFactor; smallerFactor++)
+            {
+                TryAllBiggerFactors(smallerFactor);
+            }
+        }
+
+        private static void TryAllBiggerFactors(int smallerFactor)
+        {
+            for (int biggerFactor = smallerFactor; biggerFactor <= _upperBoundFactor; biggerFactor++)
+            {
+                int palindromeCandidate = smallerFactor*biggerFactor;
+
+                if (NumberIsPalindrome(palindromeCandidate))
+                {
+                    SafeIfBiggerThenLastSaved(palindromeCandidate);
+                }
+            }
+        }
+
+        private static void SafeIfBiggerThenLastSaved(int palindromeCandidate)
+        {
+            if (IsBiggerThenLastSaved(palindromeCandidate))
+            {
+                _palindrome = palindromeCandidate;
+            }
+        }
+
+        private static bool IsBiggerThenLastSaved(int palindromeCandidate)
+        {
+            return palindromeCandidate > _palindrome;
+        }
+
+        private static bool NumberIsPalindrome(int palindromeCandidate)
+        {
+            return PalindromeChecker.NumberIsPalindrome(palindromeCandidate);
+        }
+
+        private static void SetBoundsForFactors()
+        {
+            _lowerBoundFactor = TenToThePowerOf(_digitsPerFactor-1);
+            _upperBoundFactor = TenToThePowerOf(_digitsPerFactor) - 1;
+        }
+
+        private static int TenToThePowerOf(int exponent)
+        {
+            int number = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                number *= 10;
+            }
+
+            return number;
         }
     }
 }
