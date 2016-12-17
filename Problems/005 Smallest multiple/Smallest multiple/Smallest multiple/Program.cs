672ffecb84de155b84db579991a07f56fc547030
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-17 23:45:16 +0100

    005: Added problem description.

--- Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Program.cs
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//https://projecteuler.net/problem=5
+
+//Smallest multiple
+//Problem 5
+
+//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
 namespace Smallest_multiple
 {
     class Program
