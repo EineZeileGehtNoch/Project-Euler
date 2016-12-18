c6c89aa685bf2f20c7ebba7539851fc75b3f438d
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-18 11:59:46 +0100

    005: Added SafeInput

--- Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/PrimeFactorGenerator.cs
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/PrimeFactorGenerator.cs
@@ -8,12 +8,21 @@ namespace Smallest_multiple
     class PrimeFactorGenerator
     {
         private static List<int> _primeFactors;
+        private static int _number;
 
         public static List<int> GetNumberOfPrimeFactors(int number)
         {
+            SafeInput(number);
             CreateEmptyPrimeFactors();
 
+
+
             return _primeFactors;
+        }
+
+        private static void SafeInput(int number)
+        {
+            _number = number;
         }
 
         private static void CreateEmptyPrimeFactors()
