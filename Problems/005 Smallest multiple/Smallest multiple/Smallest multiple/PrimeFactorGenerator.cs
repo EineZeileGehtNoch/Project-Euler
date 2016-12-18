5d5c359409bc9877e51b22f3436e5f38c5498300
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-18 11:09:34 +0100

    005: Created PrimeFactorGenerator Class.

---
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/PrimeFactorGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smallest_multiple
+{
+    class PrimeFactorGenerator
+    {
+        List<int> GetNumberOfPrimeFactors()
+        {
+            return null;
+        }
+    }
+}
