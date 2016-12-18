301ded874428d7a9180f0f4514a35bbe04e05011
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-18 11:22:51 +0100

    005: Added NoPrimeFactors TestMethod.

--- Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Test/PrimeFactorGeneratorTest.cs
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Test/PrimeFactorGeneratorTest.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Smallest_multiple.Test
 {
-    class PrimeFactorGeneratorTest
+    [TestClass]
+    public class PrimeFactorGeneratorTest
     {
+        [TestMethod]
+        public void NoPrimeFactors()
+        {
+            int number = 1;
+            List<int> expected = new List<int>();
 
+            List<int> actual = PrimeFactorGenerator.GetNumberOfPrimeFactors();
+
+            CollectionAssert.AreEqual(expected,actual);
+        }
     }
 }
