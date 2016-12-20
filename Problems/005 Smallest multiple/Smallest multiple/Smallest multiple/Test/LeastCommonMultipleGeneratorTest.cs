b4cab9686bd9931f884464a7639c9d0679ff3937
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-20 20:45:47 +0100

    005: Wrote BigLeastCommonMultiple TestMethod.

--- Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Test/LeastCommonMultipleGeneratorTest.cs
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Test/LeastCommonMultipleGeneratorTest.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web.Security;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Smallest_multiple.Test
 {
     [TestClass]
-    public class SmallestMultipleGeneratorTest
+    public class LeastCommonMultipleGeneratorTest
     {
         [TestMethod]
         public void TrivialSmallestMultiple()
@@ -15,7 +16,7 @@ namespace Smallest_multiple.Test
             List<int> numbers = new List<int> {1};
             int expected = 1;
 
-            int actual = SmallestMultipleGenerator.GetSmallestMultipleOf(numbers);
+            int actual = LeastCommonMultipleGenerator.GetLCMOf(numbers);
 
             Assert.AreEqual(expected, actual);
         }
@@ -26,7 +27,18 @@ namespace Smallest_multiple.Test
             List<int> numbers = new List<int> {2, 3, 5};
             int expected = 2*3*5;
 
-            int actual = SmallestMultipleGenerator.GetSmallestMultipleOf(numbers);
+            int actual = LeastCommonMultipleGenerator.GetLCMOf(numbers);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void BigLeastCommonMultiple()
+        {
+            List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int expected = 2520;
+
+            int actual = LeastCommonMultipleGenerator.GetLCMOf(numbers);
 
             Assert.AreEqual(expected,actual);
         }
