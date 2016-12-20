d6812a778004909d551d033229ba76ce2f500936
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-20 13:25:46 +0100

    005: Wrote TrivialSmallestMultiple TestMethod.

--- Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Test/SmallestMultipleGeneratorTest.cs
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/Test/SmallestMultipleGeneratorTest.cs
@@ -9,5 +9,26 @@ namespace Smallest_multiple.Test
     [TestClass]
     public class SmallestMultipleGeneratorTest
     {
+        [TestMethod]
+        public void TrivialSmallestMultiple()
+        {
+            List<int> numbers = new List<int> {1};
+            int expected = 1;
+
+            int actual = SmallestMultipleGenerator.GetSmallestMultipleOf(numbers);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SmallestMultipleOfPrimes()
+        {
+            List<int> numbers = new List<int> {2, 3};
+            int expected = 2*3;
+
+            int actual = SmallestMultipleGenerator.GetSmallestMultipleOf(numbers);
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
