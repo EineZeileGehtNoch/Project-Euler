95cd4a99064bbe2235bc6db077d17c32cd733cf4
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-20 13:11:40 +0100

    005: Created SmallestMultipleGeneratorClass and GetSmallestMultipleOf Method.

---
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/SmallestMultipleGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smallest_multiple
+{
+    class SmallestMultipleGenerator
+    {
+        public int GetSmallestMultipleOf(List<int> numbers)
+        {
+            return 1;
+        }
+    }
+}
