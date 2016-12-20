02cb033dfe857410f0da037200eaaee43bc5b42d
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-20 14:00:47 +0100

    005: Partially wrote GetSmallestMultipleOf Method.

--- Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/SmallestMultipleGenerator.cs
+++ Problems/005 Smallest multiple/Smallest multiple/Smallest multiple/SmallestMultipleGenerator.cs
@@ -7,9 +7,57 @@ namespace Smallest_multiple
 {
     class SmallestMultipleGenerator
     {
-        public int GetSmallestMultipleOf(List<int> numbers)
+        private static List<int> _numbers;
+        private static List<List<int>> _numberOfPrimesFactorsOfNumbers;
+        private static List<int> _smallesMultiplNumberOfPrimeFactors;
+
+        public static int GetSmallestMultipleOf(List<int> numbers)
         {
+            SaveNumbers(numbers);
+
+            _numberOfPrimesFactorsOfNumbers = new List<List<int>>();
+            foreach (int number in numbers)
+            {
+                List<int> numberOfPrimeFactors = PrimeFactorGenerator.GetNumberOfPrimeFactors(number);
+                _numberOfPrimesFactorsOfNumbers.Add(numberOfPrimeFactors);
+            }
+
+            _smallesMultiplNumberOfPrimeFactors = new List<int>();
+
+            foreach (var numberOfPrimesFactorsOfNumber in _numberOfPrimesFactorsOfNumbers)
+            {
+                while (PrimeFactorsNeedToBeAdded(numberOfPrimesFactorsOfNumber))
+                {
+                AddPrimeFactor();
+                }
+                foreach (List<int> primesFactorsOfNumber in _numberOfPrimesFactorsOfNumbers)
+                {
+                    for (int primeIndex = 0; primeIndex < primesFactorsOfNumber.Count; primeIndex++)
+                    {
+                        if (_smallesMultiplNumberOfPrimeFactors[primeIndex]<primesFactorsOfNumber[primeIndex])
+                        {
+                            _smallesMultiplNumberOfPrimeFactors[primeIndex] = primesFactorsOfNumber[primeIndex];
+                        }
+                    }
+                }
+            }
+
             return 1;
+        }
+
+        private static void AddPrimeFactor()
+        {
+            _smallesMultiplNumberOfPrimeFactors.Add(0);
+        }
+
+        private static bool PrimeFactorsNeedToBeAdded(List<int> numberOfPrimesFactorsOfNumber)
+        {
+            return numberOfPrimesFactorsOfNumber.Count>_smallesMultiplNumberOfPrimeFactors.Count;
+        }
+
+        private static void SaveNumbers(List<int> numbers)
+        {
+            _numbers = numbers;
         }
     }
 }
