fc8cc25f45e1f783054b3cc1cfc4a0e0cd8622a9
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-23 00:58:58 +0100

    006: Added problem description.

--- Problems/006 Sum square difference/Sum square difference/Sum square difference/Program.cs
+++ Problems/006 Sum square difference/Sum square difference/Sum square difference/Program.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//https://projecteuler.net/problem=6
+
+//Sum square difference
+//Problem 6
+
+//The sum of the squares of the first ten natural numbers is,
+//12 + 22 + ... + 102 = 385
+
+//The square of the sum of the first ten natural numbers is,
+//(1 + 2 + ... + 10)2 = 552 = 3025
+
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
 namespace Sum_square_difference
 {
     class Program
