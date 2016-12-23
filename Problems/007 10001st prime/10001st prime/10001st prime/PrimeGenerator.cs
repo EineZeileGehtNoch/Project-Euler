c9f3d51efe22bcc947503a41d914844c099c1d5b
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-24 00:39:43 +0100

    007: Created PrimeGenerator Class.

---
+++ Problems/007 10001st prime/10001st prime/10001st prime/PrimeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10001st_prime
+{
+    static class PrimeGenerator
+    {
+        public static int GetNthPrime(int numberOfPrime)
+        {
+            return 0;
+        }
+    }
+}
