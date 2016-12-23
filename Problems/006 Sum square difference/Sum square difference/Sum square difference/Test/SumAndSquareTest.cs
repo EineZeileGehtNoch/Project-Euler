3080496c0dd44eb499ea68abaa0fccf7609a9527
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-23 01:48:39 +0100

    006: TrivialDifference TestMethod.

--- Problems/006 Sum square difference/Sum square difference/Sum square difference/Test/SumAndSquareTest.cs
+++ Problems/006 Sum square difference/Sum square difference/Sum square difference/Test/SumAndSquareTest.cs
@@ -8,8 +8,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Sum_square_difference.Test
 {
     [TestClass]
-    class SumAndSquareTest
+    public class SumAndSquareTest
     {
-            
+        [TestMethod]
+        public void TivialDifference()
+        {
+            int upperBound = 0;
+            int expected = 0;
+
+            int actual = SumAndSquare.DifferenceBetweenDifferentOrder(upperBound);
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
