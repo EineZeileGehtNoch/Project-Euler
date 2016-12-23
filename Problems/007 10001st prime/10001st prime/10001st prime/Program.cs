880a8fc30df954384750356411216d8db876d9fa
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-23 10:42:01 +0100

    007: Added problem description.

--- Problems/007 10001st prime/10001st prime/10001st prime/Program.cs
+++ Problems/007 10001st prime/10001st prime/10001st prime/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//https://projecteuler.net/problem=7
+
+//10001st prime
+//Problem 7
+
+//By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+//What is the 10 001st prime number?
+
+
 namespace _10001st_prime
 {
     class Program
