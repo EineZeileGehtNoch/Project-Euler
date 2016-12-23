63ddea9d693772087656aac4a2f73438864d785c
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-24 00:44:10 +0100

    007: Created PrimeGeneratorTest

---
+++ Problems/007 10001st prime/10001st prime/10001st prime/Test/PrimeGeneratorTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _10001st_prime.Test
+{
+    [TestClass]
+    public class PrimeGeneratorTest
+    {
+    }
+}
