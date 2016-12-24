1fce0a818346ecb538d0bd58fb93f4781cebb47c
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2016-12-25 00:26:17 +0100

    008: Created TestClass.

---
+++ Problems/008 Largest product in a series/Largest product in a series/Largest product in a series/Test/MagicStringUserTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Largest_product_in_a_series.Test
+{
+    [TestClass]
+ public    class MagicStringUserTest
+    {
+    }
+}
