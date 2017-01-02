bd4561db102bfaf11074525afd23f0aabc495737
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-03 00:54:44 +0100

    009: Wrote SumOfTwelve TestMethod.

---
+++ Problems/009 Special Pythagorean triplet/Special Pythagorean triplet/Special Pythagorean triplet/Test/PythagoreanTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Special_Pythagorean_triplet.Test
+{
+    [TestClass]
+    class PythagoreanTest
+    {
+        [TestMethod]
+        public void SumOfTwelve()
+        {
+            int sum = 12;
+            List<int> integerList = new List<int>{3,4,5};
+            IntegerTuple expected = new IntegerTuple(integerList);
+
+            IntegerTuple actual = Pythagorean.GetSmallestTripletWithSumOf(sum);
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
