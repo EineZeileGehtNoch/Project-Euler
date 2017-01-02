c0da09b53cc1bf2b598e606130b9edb8b4703189
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-02 02:16:53 +0100

    009: Wrote ProductEmptyTuple TestMethod.

--- Problems/009 Special Pythagorean triplet/Special Pythagorean triplet/Special Pythagorean triplet/Test/IntegerTupleTest.cs
+++ Problems/009 Special Pythagorean triplet/Special Pythagorean triplet/Special Pythagorean triplet/Test/IntegerTupleTest.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualSt
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Special_Pythagorean_triplet.Test
 {
+    [TestClass]
     public class IntegerTupleTest
     {
+        [TestMethod]
+        public void ProductEmptyTuple()
+        {
+            List<int> integers = new List<int>();
+            IntegerTuple integegTuple = new IntegerTuple(integers);
+            int expected = 0;
+
+            int actual = integegTuple.GetProduct();
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
