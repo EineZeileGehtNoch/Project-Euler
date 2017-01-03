788984c154b0b8ca4492d904093aa4e1729ca758
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-03 22:08:43 +0100

    009: Refactoring and bugfixing in GetTripletWithSumOf Method.

--- Problems/009 Special Pythagorean triplet/Special Pythagorean triplet/Special Pythagorean triplet/Pythagorean.cs
+++ Problems/009 Special Pythagorean triplet/Special Pythagorean triplet/Special Pythagorean triplet/Pythagorean.cs
@@ -2,9 +2,10 @@
 
 namespace Special_Pythagorean_triplet
 {
-    class Pythagorean
+    static class Pythagorean
     {
         private static int _sum;
+        private static IntegerTuple _pythagoreanTriplet;
 
         public const int SMALLEST_INTEGER = 1;
 
@@ -12,26 +13,61 @@ namespace Special_Pythagorean_triplet
         {
             SaveSum(sum);
 
+            FindPythagoreanTriplet();
+
+            return _pythagoreanTriplet;
+        }
+
+        private static void FindPythagoreanTriplet()
+        {
             for (int a = SMALLEST_INTEGER; a <= _sum; a++)
             {
-                for (int b = a+1; b < _sum; b++)
-                {
-                    for (int c = SMALLEST_INTEGER; c < _sum; c++)
-                    {
-
-                        if (a*a + b*b == c*c)
-                        {
-
-                        }
-   
-                    }
-
-                }
-                
+                TryAllBsAndCs(a);
             }
+        }
 
+        private static void TryAllBsAndCs(int a)
+        {
+            for (int b = a + 1; b <= _sum; b++)
+            {
+                TryAllCs(a, b);
+            }
+        }
 
-            return null;
+        private static void TryAllCs(int a, int b)
+        {
+            for (int c = SMALLEST_INTEGER; c <= _sum; c++)
+            {
+                SaveIfTripletFullfillsConditions(a, b, c);
+            }
+        }
+
+        private static void SaveIfTripletFullfillsConditions(int a, int b, int c)
+        {
+            if (FullfillsConditions(a, b, c))
+            {
+                SaveTriplet(a, b, c);
+            }
+        }
+
+        private static bool FullfillsConditions(int a, int b, int c)
+        {
+            return IsPythagoreanTriplet(a, b, c)&& HasCorrectSum(a, b, c);
+        }
+
+        private static bool HasCorrectSum(int a, int b, int c)
+        {
+            return a+b+c == _sum;
+        }
+
+        private static void SaveTriplet(int a, int b, int c)
+        {
+            _pythagoreanTriplet = new IntegerTuple(a, b, c);
+        }
+
+        private static bool IsPythagoreanTriplet(int a, int b, int c)
+        {
+            return a*a + b*b == c*c;
         }
 
         private static void SaveSum(int sum)
