9260bf2426e2ecee866e7006352f35a6e92d0e1f
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-04 10:38:57 +0100

    010: Added problem description.

--- Problems/010 Summation of primes/Summation of primes/Summation of primes/Program.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Program.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//https://projecteuler.net/problem=10
+
+//Summation of primes
+//Problem 10
+
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+//Find the sum of all the primes below two million.
+
 
 namespace Summation_of_primes
 {
