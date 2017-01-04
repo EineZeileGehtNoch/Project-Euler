ccf6c4314be870d565e930000ae6b6ccfd5fbd2e
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-04 13:18:52 +0100

    010: Wrote GetFirstNPrimes Method.

--- Problems/010 Summation of primes/Summation of primes/Summation of primes/Prime.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Prime.cs
@@ -8,5 +8,69 @@ namespace Summation_of_primes
 {
     static class Prime
     {
+        private static List<int> _primes;
+        private static readonly int FIRST_PRIME=2;
+
+
+        public static List<int> GetFirstNPrimes(int number)
+        {
+            CreatePrimesList(number);
+
+            return _primes;
+        }
+
+        private static void CreatePrimesList(int number)
+        {
+            CreateEmptyPrimeList();
+
+            FindPrimes(number);
+        }
+
+        private static void FindPrimes(int number)
+        {
+            while (MorePrimesNeeded(number))
+            {
+                FindNextPrime();
+            }
+        }
+
+        private static void FindNextPrime()
+        {
+            for (int candidate = FIRST_PRIME;; candidate++)
+            {
+                if (IsPrime(candidate))
+                {
+                    _primes.Add(candidate);
+                    break;
+                }
+            }
+        }
+
+        private static void CreateEmptyPrimeList()
+        {
+            _primes = new List<int>();
+        }
+
+        private static bool MorePrimesNeeded(int number)
+        {
+            return _primes.Count<number;
+        }
+
+        private static bool IsPrime(int candidate)
+        {
+            foreach (int prime in _primes)
+            {
+                if (FirstIsDivisibleBySecond(candidate, prime))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool FirstIsDivisibleBySecond(int candidate, int prime)
+        {
+            return candidate%prime==0;
+        }
     }
 }
--- Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/PrimeTest.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/PrimeTest.cs
@@ -8,7 +8,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Summation_of_primes.Test
 {
     [TestClass]
-    class PrimeTest
+    public class PrimeTest
     {
+        [TestMethod]
+        public void GetFirstPrime()
+        {
+            int numberOfPrimes = 1;
+            List<int> expected = new List<int>{2};
+
+            List<int> actual = Prime.GetFirstNPrimes(numberOfPrimes);
+
+            CollectionAssert.AreEqual(expected,actual);
+        }
     }
 }
