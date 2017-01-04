f7c191948ab35882e794694b080403fc7cfa5f46
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-04 21:48:13 +0100

    010: Added Product Method.

--- Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/ListIntExtension.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/ListIntExtension.cs
@@ -8,5 +8,38 @@ namespace Summation_of_primes.Test
 {
     static class ListIntExtension
     {
+        private static int _product;
+
+        public static int Product(this List<int> factors)
+        {
+            ComputeProduct(factors);
+
+            return _product;
+        }
+
+        private static void ComputeProduct(List<int> factors)
+        {
+            SetProductToDefault();
+
+            AddFactorsToProduct(factors);
+        }
+
+        private static void AddFactorsToProduct(List<int> factors)
+        {
+            foreach (int factor in factors)
+            {
+                AddToProduct(factor);
+            }
+        }
+
+        private static void AddToProduct(int factor)
+        {
+            _product *= factor;
+        }
+
+        private static void SetProductToDefault()
+        {
+            _product = 1;
+        }
     }
 }
