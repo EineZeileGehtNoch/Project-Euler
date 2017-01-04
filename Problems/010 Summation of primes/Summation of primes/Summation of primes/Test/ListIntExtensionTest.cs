da10891607b64cecd9dedd1b1916e35465303656
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-04 21:58:34 +0100

    010: Added ProductOfSomeFactors TestMethod.

--- Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/ListIntExtensionTest.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/ListIntExtensionTest.cs
@@ -11,5 +11,15 @@ namespace Summation_of_primes.Test
     [TestClass]
     public class ListIntExtensionTest
     {
+        [TestMethod]
+        public void ProductOfSomeFactors()
+        {
+            List<int> factors = new List<int> {2, 3, 4};
+            int expected = 2*3*4;
+
+            int actual = factors.Product();
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
