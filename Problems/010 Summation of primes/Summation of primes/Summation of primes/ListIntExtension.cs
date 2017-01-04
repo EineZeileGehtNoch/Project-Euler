9f5f84fa7aa711560f7031f9c4b982163f155ff8
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-04 21:53:42 +0100

    010: Created ListIntExtensionTest Class.

--- Problems/010 Summation of primes/Summation of primes/Summation of primes/ListIntExtension.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/ListIntExtension.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace Summation_of_primes.Test
+namespace Summation_of_primes
 {
     static class ListIntExtension
     {
---
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/ListIntExtensionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Summation_of_primes.Test
+{
+    [TestClass]
+    public class ListIntExtensionTest
+    {
+    }
+}
