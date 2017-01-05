4c262453dcfd2f5213dff0afc4c60415e9ebb281
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-05 03:16:28 +0100

    010: Removed unused directives  and solved problem.

--- Problems/010 Summation of primes/Summation of primes/Summation of primes/Program.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+
 //https://projecteuler.net/problem=10
 
 //Summation of primes
--- Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/ListIntExtensionTest.cs
+++ Problems/010 Summation of primes/Summation of primes/Summation of primes/Test/ListIntExtensionTest.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Summation_of_primes.Test
