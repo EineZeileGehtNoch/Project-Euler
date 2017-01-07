4115f01da6446d7003804c415b8d06e931809043
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-07 10:35:58 +0100

    011: Added FirstInteger TestMethod.

---
+++ Problems/011 Largest product in a grid/Largest product in a grid/Largest product in a grid/Test/MagicStringContainerTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Largest_product_in_a_grid.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Largest_product_in_a_grid.Test
+{
+    [TestClass]
+    public class MagicStringContainerTest
+    {
+        [TestMethod]
+        public void FirstInteger()
+        {
+            List<List<int>> integers = MagicStringContainer.GetAsIntegers();
+            int expected = 8;
+
+            int actual = integers[0][0];
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
