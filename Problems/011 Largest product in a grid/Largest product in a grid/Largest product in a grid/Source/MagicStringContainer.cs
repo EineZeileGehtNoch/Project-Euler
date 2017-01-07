d3ca98db470c48aabed104c2c852f15c6fa9b780
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-07 10:17:27 +0100

    011: Created MagicStringContainer class.

---
+++ Problems/011 Largest product in a grid/Largest product in a grid/Largest product in a grid/Source/MagicStringContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_product_in_a_grid.Source
+{
+    class MagicStringContainer
+    {
+    }
+}
