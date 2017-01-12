d4132bbfa47677802b4f660c27b3b662c424161b
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-12 18:28:12 +0100

    011: Created MatrixContainer class and public int GetLargestProduct Method.

---
+++ Problems/011 Largest product in a grid/Largest product in a grid/Largest product in a grid/Source/MatrixContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_product_in_a_grid.Source
+{
+    class MatrixContainer
+    {
+        private int[][] _matrix;
+
+        enum Directions
+        {
+            Down=0x10,
+            Right=0x01,
+            DiagonalDownAndRight=0x11
+        }
+
+        public MatrixContainer(int[][] matrix)
+        {
+            _matrix = matrix;
+        }
+
+        public int GetLargestProduct(int numberOfFactors)
+        {
+            return 0;
+        }
+
+    }
+}
