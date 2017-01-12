cb51a20842cc9bb3a218dc14fd0a5d76dda3227d
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2017-01-12 18:36:18 +0100

    011: Wrote SingleNumber TestMethod.

--- Problems/011 Largest product in a grid/Largest product in a grid/Largest product in a grid/Test/MatrixContainerTest.cs
+++ Problems/011 Largest product in a grid/Largest product in a grid/Largest product in a grid/Test/MatrixContainerTest.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Largest_product_in_a_grid.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Largest_product_in_a_grid.Test
 {
     [TestClass]
     public class MatrixContainerTest
     {
+        [TestMethod]
+        public void SingleNumber()
+        {
+            int numberOfFactors = 1;
+            int[][] matrix = new int[1][];
+            matrix[0]= new int[]{9};
+            MatrixContainer matrixContainer = new MatrixContainer(matrix);
+            int expected = 9;
 
+            int actual = matrixContainer.GetLargestProduct(numberOfFactors);
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
